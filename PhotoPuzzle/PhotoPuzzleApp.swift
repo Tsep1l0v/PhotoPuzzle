7521915ed8dec8749b72953f3ed2896609a2def5
Tsep1l0v <w1nd0wstsepilov@yandex.ru>
2024-10-15 12:52:32 +0300

    Initial Commit

---
+++ PhotoPuzzle/PhotoPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPuzzleApp.swift
+//  PhotoPuzzle
+//
+//  Created by Дмитрий Цепилов on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoPuzzleTests/PhotoPuzzleTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPuzzleTests.swift
+//  PhotoPuzzleTests
+//
+//  Created by Дмитрий Цепилов on 15.10.2024.
+//
+
+import Testing
+@testable import PhotoPuzzle
+
+struct PhotoPuzzleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
